37732253936edccbb72ba5df72bbd363dd18d874
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2021-01-07 12:04:11 +0530

    added UserController and login register views

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodexEvents.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult ShowLogin()
+        {
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult ShowReg()
+        {
+            return RedirectToAction("Register");
+        }
+    }
+}
