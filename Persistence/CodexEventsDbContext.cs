3c1672f9c2698c19a4ec01b2a1f31aa7b88e3d38
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2021-01-07 18:06:19 +0530

    defined DataContext and added InitialMigration

---
+++ Persistence/CodexEventsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodexEvents.Persistence
+{
+    public class CodexEventsDbContext: DbContext
+    {
+        public CodexEventsDbContext(DbContextOptions<CodexEventsDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
