727aa0b473101ca7d0146accf4018b98e19e0124
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2021-01-07 11:18:50 +0530

    added DataContext and enable-migrations

---
+++ Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodexEvents.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+
+        }
+    }
+}
